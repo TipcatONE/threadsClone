a8befb5a6ab7be19f9306b68fcb52500b69c74e1
Nikita Igolkin <nigolkin97@gmail.com>
2023-10-31 17:02:42 +0600

    1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -23,5 +23,5 @@ func searchInsert(_ nums: [Int], _ target: Int)->Int{
     }
         return counter
 }
-searchInsert([1,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 2)
+searchInsert([1,3,5,6,7,8,9,10], 2)
 
